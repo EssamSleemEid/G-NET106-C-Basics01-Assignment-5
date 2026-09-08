c4f2512c042276526c2beab803b66d5dfa06757d
Nongoogle <essamsleem9@gmail.com>
2026-09-08 20:38:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace fourth_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
