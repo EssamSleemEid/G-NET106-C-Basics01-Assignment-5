a01536581b1f46058af099260a4c62e4573dd2aa
Nongoogle <essamsleem9@gmail.com>
2026-09-08 20:47:24 +0300

    Question01

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace fourth_assignment
+﻿using System.Runtime.InteropServices;
+
+namespace fourth_assignment
 {
     internal class Program
     {
+        class book
+        {
+            private string password = "secret";
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question1
+            //Add a private string password = "secret"; field to a Book class. Try to print it from Main (outside the class). What happens, and why?
+
+            //book book01=new book();
+            //Console.WriteLine(book.password);      // it will not work bc it is private and you can access to it only in book scope
+            #endregion
+
+
         }
     }
 }
