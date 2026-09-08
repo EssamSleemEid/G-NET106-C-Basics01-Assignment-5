9a21ecca819f6812176b96176917cfde2dcfcae9
Nongoogle <essamsleem9@gmail.com>
2026-09-08 20:51:14 +0300

    Question02

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace fourth_assignment
         class book
         {
             private string password = "secret";
-
+            internal int copiesInStock = 5;
         }
         static void Main(string[] args)
         {
@@ -18,7 +18,12 @@ namespace fourth_assignment
             //Console.WriteLine(book.password);      // it will not work bc it is private and you can access to it only in book scope
             #endregion
 
+            #region Question2
+            //Add an internal int copiesInStock = 5; field to Book. Print it from Main. Does it compile? Why? 
 
+            //book book = new book();
+            //Console.WriteLine(book.copiesInStock);    // it will work bc its internal that can be accessed in the same project  
+            #endregion
         }
     }
 }
