9a797eff7a88c4db9bbe679936b206c185c657aa
Nongoogle <essamsleem9@gmail.com>
2026-09-08 21:55:07 +0300

    Question03

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace fourth_assignment
         {
             private string password = "secret";
             internal int copiesInStock = 5;
+            public string Title = "essam sleem";
         }
         static void Main(string[] args)
         {
@@ -21,9 +22,18 @@ namespace fourth_assignment
             #region Question2
             //Add an internal int copiesInStock = 5; field to Book. Print it from Main. Does it compile? Why? 
 
-            //book book = new book();
-            //Console.WriteLine(book.copiesInStock);    // it will work bc its internal that can be accessed in the same project  
+            //book book01 = new book();
+            //Console.WriteLine(book01.copiesInStock);    // it will work bc its internal that can be accessed in the same project  
             #endregion
+
+            #region Question3
+            //Add a public string Title; field to Book. Set it and print it from Main.
+
+            //book book01 = new book();
+            //Console.WriteLine(book01.Title);
+            #endregion
+
+
         }
     }
 }
